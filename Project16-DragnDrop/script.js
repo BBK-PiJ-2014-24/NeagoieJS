8a8d256e81967dd0d0f4cdee405469ba1d812c3f
Stewart Newnham <git@newnham.com>
2020-12-30 17:25:10 +0800

    completed project

--- Project16-DragnDrop/script.js
+++ Project16-DragnDrop/script.js
@@ -1,27 +1,41 @@
+// DOM ELEMENTS
+// ============
 const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
 const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list');
+const listColumns = document.querySelectorAll('.drag-item-list');
 const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
 const onHoldList = document.getElementById('on-hold-list');
 
-// Items
 
+// GLOBAL VARIABLES
+// ================= 
+
+// Items 
+let updatedOnLoad = false;
 
 // Initialize Arrays
 let backlogListArray = [];
 let progressListArray = [];
 let completeListArray = [];
 let onHoldListArray = [];
+let listArrays = []; // Array of all arrays
 
 // Drag Functionality
+let draggedItem;
+let currentColumn;
+let dragging = false;
+
+// FUNCTIONS
+// =========
 
 
-// Get Arrays from localStorage if available, set default values if not
+// getSavedColumns - Get Arrays from localStorage if available, set default values if not
+// ---------------
 function getSavedColumns() {
   if (localStorage.getItem('backlogItems')) {
     backlogListArray = JSON.parse(localStorage.backlogItems);
@@ -36,40 +50,180 @@ function getSavedColumns() {
   }
 }
 
-// Set localStorage Arrays
+
+// UpdateSavedColumns - Set localStorage Arrays
+// ------------------
 function updateSavedColumns() {
-  localStorage.setItem('backlogItems', JSON.stringify(backlogListArray));
-  localStorage.setItem('progressItems', JSON.stringify(progressListArray));
-  localStorage.setItem('completeItems', JSON.stringify(completeListArray));
-  localStorage.setItem('onHoldItems', JSON.stringify(onHoldListArray));
+  listArrays = [backlogListArray, progressListArray, completeListArray, onHoldListArray];
+  const arrayNames = ['backlog', 'progress', 'complete', 'onHold'];
+  arrayNames.forEach((arrName, index)=>{
+    localStorage.setItem(`${arrName}Items`, JSON.stringify(listArrays[index]));
+  });  
+
+  // localStorage.setItem('backlogItems', JSON.stringify(backlogListArray));
+  // localStorage.setItem('progressItems', JSON.stringify(progressListArray));
+  // localStorage.setItem('completeItems', JSON.stringify(completeListArray));
+  // localStorage.setItem('onHoldItems', JSON.stringify(onHoldListArray));
 }
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
-  console.log('columnEl:', columnEl);
-  console.log('column:', column);
-  console.log('item:', item);
-  console.log('index:', index);
+  // console.log('columnEl:', columnEl);
+  // console.log('column:', column);
+  // console.log('item:', item);
+  // console.log('index:', index);
   // List Item
   const listEl = document.createElement('li');
+  listEl.textContent = item;
+  listEl.id = index;
   listEl.classList.add('drag-item');
+  listEl.draggable = true; // make draggable
+  listEl.setAttribute('onfocusout', `updateItem(${index}, ${column})`);
+  listEl.setAttribute('ondragstart', 'drag(event)');
+  listEl.contentEditable = true;
+  columnEl.appendChild(listEl);
+}
 
+function filterArray(arr) {
+  // console.log(arr);
+  const filterArr = arr.filter((item) => item !==null);
+  // console.log(arr);
+  return filterArr;
+}
+
+// drag() 
+// ------
+function drag(e){
+  draggedItem = e.target;
+  dragging = true;
+}
+
+function allowDrop(e){
+  e.preventDefault();
+}
+
+function dragEnter(column){
+  // console.log(listColumns[column]);
+  listColumns[column].classList.add('over');
+  currentColumn = column;
+}
+
+function drop(e){
+  e.preventDefault();
+  listColumns.forEach((column)=>{
+    column.classList.remove('over');
+  });
+  const parentEl = listColumns[currentColumn];
+  parentEl.appendChild(draggedItem);
+  dragging = false;
+  rebuildArrays();
 }
 
 // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
 function updateDOM() {
   // Check localStorage once
-
+  if(!updatedOnLoad){
+    getSavedColumns();
+  }
   // Backlog Column
+  backlogList.textContent = ''; // remove elements
+  backlogListArray.forEach((backlogItem, index)=>{
+    createItemEl(backlogList, 0, backlogItem, index);
+  });
+  backlogListArray = filterArray(backlogListArray);
 
   // Progress Column
-
+  progressList.textContent = ''; // remove elements
+  progressListArray.forEach((progressItem, index)=>{
+    createItemEl(progressList, 1, progressItem, index);
+  });
+  progressListArray = filterArray(progressListArray);
+  
   // Complete Column
-
+  completeList.textContent = ''; // remove elements
+  completeListArray.forEach((completeItem, index)=>{
+    createItemEl(completeList, 2, completeItem, index);
+  });
+  completeListArray = filterArray(completeListArray);
+  
+  
   // On Hold Column
-
+  onHoldList.textContent = ''; // remove elements
+  onHoldListArray.forEach((onHoldItem, index)=>{
+    createItemEl(onHoldList, 3, onHoldItem, index);
+  });
+  onHoldListArray = filterArray(onHoldListArray);
+  
   // Run getSavedColumns only once, Update Local Storage
-
-
+  updatedOnLoad = true;
+  updateSavedColumns();
+}
+// rebuildArrays()
+// ---------------
+function rebuildArrays(){
+  
+  backlogListArray = [];
+  for(let i=0; i<backlogList.children.length;i++){
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+ 
+  progressListArray = [];
+  for(let i=0; i<progressList.children.length;i++){
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  
+  completeListArray = [];
+  for(let i=0; i<completeList.children.length;i++){
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for(let i=0; i<onHoldList.children.length;i++){
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+  updateDOM();
 }
 
+function showInputBox(column){
+  addBtns[column].style.visibility = 'hidden';
+  saveItemBtns[column].style.display = 'flex';
+  addItemContainers[column].style.display = 'flex';
+}
+
+function addToColumn(column){
+  const  itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = '';
+  updateDOM();
+}
+
+function updateItem(id, column){
+  const selectedArray = listArrays[column];
+  const selectedColumnEl = listColumns[column].children;
+  if(!dragging){
+    if(!selectedColumnEl[id].textContent){
+      delete selectedArray[id];
+    } else {
+      selectedArray[id] = selectedColumnEl[id].textContent;
+    }
+    updateDOM();
+  }
+}
+
+function hideInputBox(column){
+  addBtns[column].style.visibility = 'visible';
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display = 'none';
+  addToColumn(column);
+}
+
+
+
+
+// EVENT LISTENERS
+// ===============
+
+
+// ON LOAD
+// =======
+updateDOM();
