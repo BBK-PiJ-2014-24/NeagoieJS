bfc609318b2177b02932bcb6f859e3b59fe0160a
Stewart Newnham <git@newnham.com>
2020-12-30 23:10:36 +0800

    refactored rebuildArrays() with map functions

--- Project16-DragnDrop/script.js
+++ Project16-DragnDrop/script.js
@@ -162,24 +162,17 @@ function updateDOM() {
 // ---------------
 function rebuildArrays(){
   
-  backlogListArray = [];
-  for(let i=0; i<backlogList.children.length;i++){
-    backlogListArray.push(backlogList.children[i].textContent);
-  }
- 
-  progressListArray = [];
-  for(let i=0; i<progressList.children.length;i++){
-    progressListArray.push(progressList.children[i].textContent);
-  }
+  backlogListArray = Array.from(backlogListEl.children).map(i => i.textContent);
+  progressListArray = Array.from(progressListEl.children).map(i => i.textContent);
+  completeListArray = Array.from(completeListEl.children).map(i => i.textContent);
+  onHoldListArray = Array.from(onHoldListEl.children).map(i => i.textContent);
   
-  completeListArray = [];
-  for(let i=0; i<completeList.children.length;i++){
-    completeListArray.push(completeList.children[i].textContent);
-  }
-  onHoldListArray = [];
-  for(let i=0; i<onHoldList.children.length;i++){
-    onHoldListArray.push(onHoldList.children[i].textContent);
-  }
+ 
+  // progressListArray = [];
+  // for(let i=0; i<progressList.children.length;i++){
+  //   progressListArray.push(progressList.children[i].textContent);
+  // }
+  
   updateDOM();
 }
 
