09800aa0c0191d831885f28311cd882a91d91300
Stewart Newnham <git@newnham.com>
2020-12-31 15:51:36 +0800

    Added comments and reorg the order of functions

--- Project16-DragnDrop/script.js
+++ Project16-DragnDrop/script.js
@@ -18,8 +18,8 @@ const onHoldList = document.getElementById('on-hold-list');
 // Items 
 let updatedOnLoad = false;
 
-// Initialize Arrays
-let backlogListArray = [];
+// Column Channel Arrays - Each elem in the array contains the text of each item
+let backlogListArray = []; 
 let progressListArray = [];
 let completeListArray = [];
 let onHoldListArray = [];
@@ -33,9 +33,11 @@ let dragging = false;
 // FUNCTIONS
 // =========
 
+// LOCAL STORAGE
+// -------------
 
-// getSavedColumns - Get Arrays from localStorage if available, set default values if not
-// ---------------
+// getSavedColumns() - Get Column Arrays from localStorage if available, set default items if not
+// -----------------
 function getSavedColumns() {
   if (localStorage.getItem('backlogItems')) {
     backlogListArray = JSON.parse(localStorage.backlogItems);
@@ -50,47 +52,18 @@ function getSavedColumns() {
   }
 }
 
-
-// UpdateSavedColumns - Set localStorage Arrays
-// ------------------
+// UpdateSavedColumns() - Update and Set localStorage Arrays, which represent each column channel
+// --------------------
 function updateSavedColumns() {
   listArrays = [backlogListArray, progressListArray, completeListArray, onHoldListArray];
   const arrayNames = ['backlog', 'progress', 'complete', 'onHold'];
   arrayNames.forEach((arrName, index)=>{
     localStorage.setItem(`${arrName}Items`, JSON.stringify(listArrays[index]));
   });  
-
-  // localStorage.setItem('backlogItems', JSON.stringify(backlogListArray));
-  // localStorage.setItem('progressItems', JSON.stringify(progressListArray));
-  // localStorage.setItem('completeItems', JSON.stringify(completeListArray));
-  // localStorage.setItem('onHoldItems', JSON.stringify(onHoldListArray));
 }
 
-// Create DOM Elements for each list item
-function createItemEl(columnEl, column, item, index) {
-  // console.log('columnEl:', columnEl);
-  // console.log('column:', column);
-  // console.log('item:', item);
-  // console.log('index:', index);
-  // List Item
-  const listEl = document.createElement('li');
-  listEl.textContent = item;
-  listEl.id = index;
-  listEl.classList.add('drag-item');
-  listEl.draggable = true; // make draggable
-  listEl.setAttribute('onfocusout', `updateItem(${index}, ${column})`);
-  listEl.setAttribute('ondragstart', 'drag(event)');
-  listEl.contentEditable = true;
-  columnEl.appendChild(listEl);
-}
-
-function filterArray(arr) {
-  // console.log(arr);
-  const filterArr = arr.filter((item) => item !==null);
-  // console.log(arr);
-  return filterArr;
-}
-
+// DRAG FUNCTIONALITY
+// ------------------
 // drag() 
 // ------
 function drag(e){
@@ -103,7 +76,6 @@ function allowDrop(e){
 }
 
 function dragEnter(column){
-  // console.log(listColumns[column]);
   listColumns[column].classList.add('over');
   currentColumn = column;
 }
@@ -116,38 +88,56 @@ function drop(e){
   const parentEl = listColumns[currentColumn];
   parentEl.appendChild(draggedItem);
   dragging = false;
-  rebuildArrays();
+  rebuildArrays(); // REBUILD THE ARRAYS AFTER ITEMS HAVE BEEN MOVED AROUND
 }
 
-// Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
+// rebuildArrays() - 
+// ---------------
+// 1. Rebuilds the Arrays after items have been moved from one column to the next 
+// 2. Update the DOM
+function rebuildArrays(){
+  
+  backlogListArray = Array.from(backlogListEl.children).map(i => i.textContent);
+  progressListArray = Array.from(progressListEl.children).map(i => i.textContent);
+  completeListArray = Array.from(completeListEl.children).map(i => i.textContent);
+  onHoldListArray = Array.from(onHoldListEl.children).map(i => i.textContent);
+ 
+  updateDOM();
+}
+
+// RENDERING LOGIC
+// ---------------
+
+// updateDOM()
+// -----------
+// 1. Check localStorage once
+// 2. For Each Channel Colum
+//    a. clear old elements
+//    b. create new elements and add to DOM
+//    c. filter out null elements in array
+//  3. update local storage
 function updateDOM() {
-  // Check localStorage once
   if(!updatedOnLoad){
     getSavedColumns();
   }
-  // Backlog Column
-  backlogList.textContent = ''; // remove elements
+  backlogList.textContent = ''; // remove  old elements
   backlogListArray.forEach((backlogItem, index)=>{
     createItemEl(backlogList, 0, backlogItem, index);
   });
-  backlogListArray = filterArray(backlogListArray);
+  backlogListArray = filterArray(backlogListArray); // remove null elements in array
 
-  // Progress Column
   progressList.textContent = ''; // remove elements
   progressListArray.forEach((progressItem, index)=>{
     createItemEl(progressList, 1, progressItem, index);
   });
   progressListArray = filterArray(progressListArray);
   
-  // Complete Column
   completeList.textContent = ''; // remove elements
   completeListArray.forEach((completeItem, index)=>{
     createItemEl(completeList, 2, completeItem, index);
   });
   completeListArray = filterArray(completeListArray);
   
-  
-  // On Hold Column
   onHoldList.textContent = ''; // remove elements
   onHoldListArray.forEach((onHoldItem, index)=>{
     createItemEl(onHoldList, 3, onHoldItem, index);
@@ -158,30 +148,55 @@ function updateDOM() {
   updatedOnLoad = true;
   updateSavedColumns();
 }
-// rebuildArrays()
-// ---------------
-function rebuildArrays(){
-  
-  backlogListArray = Array.from(backlogListEl.children).map(i => i.textContent);
-  progressListArray = Array.from(progressListEl.children).map(i => i.textContent);
-  completeListArray = Array.from(completeListEl.children).map(i => i.textContent);
-  onHoldListArray = Array.from(onHoldListEl.children).map(i => i.textContent);
-  
- 
-  // progressListArray = [];
-  // for(let i=0; i<progressList.children.length;i++){
-  //   progressListArray.push(progressList.children[i].textContent);
-  // }
-  
-  updateDOM();
+
+// function filterArray() - remove null item objects that occur after deletion 
+// ----------------------
+function filterArray(arr) {
+  const filterArr = arr.filter((item) => item !==null);
+  return filterArr;
 }
 
+
+
+// ITEM BOX
+// --------
+
+// CreateItemEl() - Funtion that constructs the DOM Elements for each list item <li>
+// --------------
+// 1. ColumnEl is the Channel Column
+// 2. Column is the index of the colum channel
+// 3. item is the textContent
+// 4. index is the order in the column array where the item has been dropped
+function createItemEl(columnEl, column, item, index) {
+  const listEl = document.createElement('li');
+  listEl.textContent = item;
+  listEl.id = index;
+  listEl.classList.add('drag-item');
+  listEl.draggable = true; // make draggable
+  listEl.setAttribute('onfocusout', `updateItem(${index}, ${column})`);
+  listEl.setAttribute('ondragstart', 'drag(event)');
+  listEl.contentEditable = true;
+  columnEl.appendChild(listEl);
+}
+// showInputBox() - reveals the textbox to edit the item
+// --------------
 function showInputBox(column){
-  addBtns[column].style.visibility = 'hidden';
-  saveItemBtns[column].style.display = 'flex';
+  addBtns[column].style.visibility = 'hidden'; // hides the "Add Item Btn"
+  saveItemBtns[column].style.display = 'flex'; // flex items are revealed when display changed from 'none'
   addItemContainers[column].style.display = 'flex';
 }
 
+// hideInputBox() - hides the textbox on the item
+// --------------
+function hideInputBox(column){
+  addBtns[column].style.visibility = 'visible'; // returns the "Add Item Btn"
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display = 'none';
+  addToColumn(column);
+}
+
+// addToColumn() - Add new item to a channel column
+// -------------
 function addToColumn(column){
   const  itemText = addItems[column].textContent;
   const selectedArray = listArrays[column];
@@ -190,6 +205,8 @@ function addToColumn(column){
   updateDOM();
 }
 
+// updateItem()
+// ------------
 function updateItem(id, column){
   const selectedArray = listArrays[column];
   const selectedColumnEl = listColumns[column].children;
@@ -203,19 +220,11 @@ function updateItem(id, column){
   }
 }
 
-function hideInputBox(column){
-  addBtns[column].style.visibility = 'visible';
-  saveItemBtns[column].style.display = 'none';
-  addItemContainers[column].style.display = 'none';
-  addToColumn(column);
-}
-
-
 
 
 // EVENT LISTENERS
-// ===============
-
+// =============== 
+// NONE - Instead, HTML onclick= attributes are inserted instead. 
 
 // ON LOAD
 // =======
